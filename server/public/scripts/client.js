5c920c3170990d12e58c73980b914a7f055cfc5b
CJ Simon <cjsimon250@outlook.com>
2023-02-02 14:55:43 -0600

    Added AJAX POST & GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,56 @@
-console.log( 'js' );
+console.log("js");
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
-  // Establish Click Listeners
-  setupClickListeners()
-  // load existing koalas on page load
+$(document).ready(function () {
+  console.log("JQ");
+  setupClickListeners();
+
   getKoalas();
-
 }); // end doc ready
 
+let koalas = [];
+
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+  $("#addButton").on("click", postKoala);
 }
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getKoalas() {
+  console.log("in getKoalas");
   // ajax call to server to get koalas
-  
+
+  $.ajax({
+    method: "GET",
+    url: "/koalas",
+  }).then((response) => {
+    koalas = response;
+
+    render();
+  });
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
+function postKoala() {
+  //console.log("in addKoala", newKoala);
   // ajax call to server to get koalas
- 
+  let koalaToSend = {
+    name: $("#nameIn").val(),
+    gender: $("#genderIn").val(),
+    age: $("#ageIn").val(),
+    readyForTransfer: $("#readyForTransferIn").val(),
+    notes: $("#notesIn").val(),
+  };
+
+  console.log(koalaToSend);
+
+  $.ajax({
+    method: "POST",
+    url: "/koalas",
+    data: koalaToSend,
+  }).then((response) => {
+    $("#nameIn").val("");
+    $("#genderIn").val(""),
+      $("#ageIn").val(""),
+      $("#readyForTransferIn").val(""),
+      $("#notesIn").val("");
+
+    getKoalas();
+  });
 }
