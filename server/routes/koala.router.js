26799e57525e82fbdbb17e660dd38e9a828b891a
Xai Hang <xaihang12@gmail.com>
2023-02-02 15:36:19 -0600

    client setup put ajax and router setup endpoint

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,36 @@
-const express = require("express");
+const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-const pool = require("../modules/pool");
+const pool = require('../modules/pool');
 
 // GET
 
 // POST
 
 // PUT
+koalaRouter.put('/:id', (req, res) => {
+  const queryText =
+    `UPDATE "koala_table" 
+    SET "ready_to_transfer"=$1
+    WHERE "id"=$2;`;
+
+
+  const queryParams = [req.body.ready_to_transfer, req.params.id];
+
+  pool
+    .query(queryText, queryParams)
+    .then((dbRes) => {
+    //   console.log('sending 200');
+    //   res.status(200).json({ success: true });
+    res.sendStatus(200); 
+    })
+    .catch((error) => {
+      console.log('sending 500');
+      console.log(`PUT Error making query: ${queryText}`, error);
+      res.sendStatus(500);
+    });
+});
 
 // DELETE
 
