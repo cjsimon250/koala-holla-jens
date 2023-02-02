4ca81de85dacfa5537d34476ec6b2579ee61d8d2
John Mayou <johnm.ceo@gmail.com>
2023-02-02 15:12:14 -0600

    wrote render function to append to koalas state to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,52 @@
-console.log( 'js' );
+console.log("js");
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
-  // Establish Click Listeners
-  setupClickListeners()
-  // load existing koalas on page load
-  getKoalas();
-
+$(document).ready(function () {
+	console.log("JQ");
+	// Establish Click Listeners
+	setupClickListeners();
+	// load existing koalas on page load
+	getKoalas();
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+	$("#addButton").on("click", function () {
+		console.log("in addButton on click");
+		// get user input and put in an object
+		// NOT WORKING YET :(
+		// using a test object
+		let koalaToSend = {
+			name: "testName",
+			age: "testName",
+			gender: "testName",
+			readyForTransfer: "testName",
+			notes: "testName",
+		};
+		// call saveKoala with the new obejct
+		saveKoala(koalaToSend);
+	});
 }
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  
+function getKoalas() {
+	console.log("in getKoalas");
+	// ajax call to server to get koalas
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
+function saveKoala(newKoala) {
+	console.log("in saveKoala", newKoala);
+	// ajax call to server to get koalas
+}
+
+function render() {
+	$("#viewKoalas").empty();
+	for (let koala of koalas) {
+		$("#viewKoalas").append(`
+      <tr data-id='${koala.id}'>
+        <td>${koala.name}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.age}</td>
+        <td>${koala.ready_to_transfer}</td>
+        <td>${koala.notes}</td>
+      </tr>
+    `);
+	}
 }
